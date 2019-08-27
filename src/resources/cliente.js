5b5f3d0f5c9a56f9c8d2ebc1cc83a9c608bde011
Wallace <wallacerheimon@gmail.com>
2019-08-27 12:28:19 -0300

    Create cliente.js

---
+++ src/resources/cliente.js
@@ -0,0 +1,33 @@
+require('../models/cliente')
+
+const mongoose = require('mongoose')
+const modelo = mongoose.model('clientes')
+
+// método CRUD
+class Cliente {
+    static async criar(dados) {
+        return await new modelo(dados).save()
+    }
+
+    static async buscarTodos() {
+        return await modelo.find({})
+    }
+
+    static async buscarPorId(id) {
+        return await modelo.findOne({_id: id})
+    }
+
+    static async atualizar(id, dados) {
+        return await modelo.findOneAndUpdateid(id, {$set: dados})
+    }
+
+    static async deletar(id) {
+        return await modelo.findOneAndRemove(id)
+    }
+
+    static async validarRegistro(dados) {
+        let 
+    }
+}
+
+module.exports = Cliente
