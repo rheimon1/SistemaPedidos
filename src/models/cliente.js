d87069f194277a87a9a47dd1c039bc8c2cc86f08
Wallace <wallacerheimon@gmail.com>
2019-08-27 12:28:13 -0300

    Create cliente.js

---
+++ src/models/cliente.js
@@ -0,0 +1,19 @@
+const smc = require('simple-mongoose-creator')
+smc.smc('clientes', {
+    nome: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    telefone: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = smc
