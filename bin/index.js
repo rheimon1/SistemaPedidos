22b68edc86e269a0417c6823d15c4fcc12b30a7b
Wallace <wallacerheimon@gmail.com>
2019-08-27 12:26:21 -0300

    Create index.js

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// Configurando o parser
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Configurando o frontend
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+// Definindo arquivos estáticos
+app.use(express.static('public'))
+
+const cliente_route = require('../src/routes/cliente')
+app.use('/cliente', cliente_route)
+
+// Chamando rotas
+app.use('/', (req, res) => {
+    return res.render('login')
+})
+
+// Exportando aplicação
+module.exports = app
